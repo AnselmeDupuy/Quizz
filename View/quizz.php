9e3ad605a2fcae43cc5f231c70ece22160c2c4ce
anselme dupuy <anselmedupuy@gmail.com>
2025-01-09 16:59:37 +0100

    modif script

--- View/quizz.php
+++ View/quizz.php
@@ -1,25 +1,18 @@
 
-<div class="row mb-3">
-  <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Quizz Name</label>
-  <div class="col-sm-10">
-    <input type="email" class="form-control form-control-lg" id="colFormLabelLg" placeholder="">
+<form method="post" id="quizz-form">
+  <div class="row mb-3">
+    <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Quizz Name</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control form-control-lg" id="quizz-name" placeholder="">
+    </div>
   </div>
-</div>
-<div class="row mb-3">
-  <label for="colFormLabel" class="col-sm-2 col-form-label">Question 1</label>
-  <div class="col-sm-10">
-    <input type="email" class="form-control" id="colFormLabel" placeholder="">
+  <div class="row mb-3">
+    <label for="colFormLabel" class="col-sm-2 col-form-label">Question 1</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="question-1" placeholder="">
+    </div>
   </div>
-</div>
-<div class="row mb-3">
-  <label for="colFormLabel" class="col-sm-2 col-form-label">Question 2</label>
-  <div class="col-sm-10">
-    <input type="email" class="form-control" id="colFormLabel" placeholder="">
-  </div>
-</div>
-<div class="row mb-3">
-  <label for="colFormLabel" class="col-sm-2 col-form-label">Question 3</label>
-  <div class="col-sm-10">
-    <input type="email" class="form-control" id="colFormLabel" placeholder="">
-  </div>
-</div>
+
+
+  <button type="submit" class="btn btn-primary" name="quizz_button" id="create-quizz-btn">Submit</button>
+</form>
--- scripts/fixtures.php
+++ scripts/fixtures.php
@@ -8,7 +8,26 @@ require  './vendor/autoload.php';
 
 $faker = Faker\Factory::create('fr_FR');
 
-for ($i = 0; $i <= 1; $i++){
+for ($i = 1; $i <= 2; $i++){
+    if ($i == 1) {$user = "admin"; $password = "admin"; $group = "2";} else {$user = "user1"; $password = "user1"; $group = "1";}
+    $query="INSERT INTO users (username, password, group_id, enabled) VALUES 
+            (:username, :password, :group_id, :enabled)";
+    $prep = $pdo->prepare($query);
+
+    $enabled = "1";
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    $prep->bindValue(':username', $user);
+    $prep->bindValue(':password', $password);
+    $prep->bindValue(':group_id', $group);
+    $prep->bindValue(':enabled', $enabled);
+
+
+    $prep->execute();
+
+}
+
+for ($i = 0; $i <= 8; $i++){
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $query="INSERT INTO users (username, password, group_id, enabled) VALUES 
             (:username, :password, :group_id, :enabled)";
@@ -34,3 +53,4 @@ for ($i = 0; $i <= 1; $i++){
     }
     $prep->closeCursor();
 }
+?>
